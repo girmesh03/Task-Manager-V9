6a44fe3b43c2268f2cc65e40ca08d105135386f1
girmesh03 <girmachewzewdie03@gmail.com>
2025-05-15 20:30:14 +0300

    add full setup of auth, department, task and user routes

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,49 @@
+import express from "express";
+
+import {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUserById,
+  deleteUserById,
+} from "../controllers/UserController.js";
+
+import { verifyJWT, authorizeRoles } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.use(verifyJWT); // Apply JWT to all following routes
+
+// Create a new user
+router.post(
+  "/department/:departmentId",
+  authorizeRoles("SuperAdmin", "Admin"),
+  createUser
+);
+
+// Get all users in a department
+router.get(
+  "/department/:departmentId",
+  authorizeRoles("SuperAdmin", "Admin", "Manager", "User"),
+  getAllUsers
+);
+
+// Get a user by ID in a department
+// TODO: who can access this endpoint?
+router.get("/department/:departmentId/user/:userId", getUserById);
+
+// Update a user by ID in a department
+router.put(
+  "/department/:departmentId/user/:userId",
+  authorizeRoles("SuperAdmin", "Admin"),
+  updateUserById
+);
+
+// Get a user by ID
+router.delete(
+  "/department/:departmentId/user/:userId",
+  authorizeRoles("SuperAdmin", "Admin"),
+  deleteUserById
+);
+
+export default router;
