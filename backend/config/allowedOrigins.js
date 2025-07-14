49ead7d45625c3dce577aa49f9445f0bc29e05ee
girmesh03 <girmachewzewdie03@gmail.com>
2025-07-14 23:43:27 +0300

    deploy test 2

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://task-manager-v9.vercel.app",
+  "https://task-manager-v9-vn1q-girmazewdei38-4801s-projects.vercel.app",
 ];
 
 export default allowedOrigins;
